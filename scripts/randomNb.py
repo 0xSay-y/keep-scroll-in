2d097f86c3288162c2dfba13df345921f797b4d2
say y <say.y4906@gmail.com>
2023-03-14 22:59:51 +0100

    06 - Smart contracts x2 + Python script

---
+++ scripts/randomNb.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import json
+import schedule
+import time
+import random
+
+def job():
+  try:
+    nb_data = {}
+    nb = random.randint(0, 4000)
+    nb_data['nb'] = nb
+
+    with open('randomNb.json', 'w', encoding='utf-8') as final_nb:
+      json.dump(nb_data, final_nb, ensure_ascii=False, indent=2)
+  except:
+    pass
+    
+schedule.every(1).seconds.do(job)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
